cf27b266f2f6049113c1f434767e4f4c8e5f2f62
abbdk160820 <bunaoaubrey@gmail.com>
2024-01-28 21:13:27 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// const body = document.body
+// const div = document.createElement('div')
+// body.append(div)
+
+// div.innerText = "Hello World"
+
+// div.innerHTML = "<b>Hello World </b>"
+
+// const strong = document.createElement("strong")
+// strong.innerText = "Hello World"
+// div.append(strong)
+// const div = document.querySelector('div')
+
+// const spanHi = document.querySelector('#hi')
+
+// const spanBye = document.querySelector('#bye')
+
+// console.log(spanHi.getAttribute("title"))
+// console.log(spanHi.id = "changed-id")
+// spanHi.removeAttribute("title")
+
+// spanHi.style.color = "blue"
+// spanHi.style.backgroundcolor = "yellow"
